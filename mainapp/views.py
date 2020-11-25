f0e554916f8db6f1f9025b548e366a49b3d13d30
preboe <preboe@yandex.ru>
2020-11-25 19:19:34 +0700

    добавлены html страници

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import DeleteView
 
-from .models import soap_from_scratch
+from .models import soap_from_scratch, baby_soap
 
 def test_view(request):
     return render(request, 'base.html', {})
@@ -11,12 +11,12 @@ class ProductDetailView(DeleteView):
 
     CT_MODEL_MODEL_CLASS = {
         'soap_from_scratch': soap_from_scratch,
-
+        'baby_soap': baby_soap,
     }
 
     def dispatch(self, request, *args, **kwargs):
         self.model = self.CT_MODEL_MODEL_CLASS[kwargs['ct_model']]
-        self.queryset = self.model._base_menager.all()
+        self.queryset = self.model._base_manager.all()
         return super().dispatch(request, *args, **kwargs)
 
     # model = Model
